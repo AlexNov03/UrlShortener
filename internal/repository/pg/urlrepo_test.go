44dd7770cba12dd7f175311ea363da935f2f2fb3
Alex Novak <shura.novak.03@mail.ru>
2025-03-02 02:43:32 +0300

    api test: add tests to repository

--- internal/repository/pg/urlrepo_test.go
+++ internal/repository/pg/urlrepo_test.go
@@ -3,16 +3,24 @@ package pg
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"net/http"
 	"testing"
 
+	"github.com/AlexNov03/UrlShortener/internal/models"
 	"github.com/AlexNov03/UrlShortener/utils"
 	"github.com/DATA-DOG/go-sqlmock"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetOriginalUrlOk(t *testing.T) {
+func TestGetOriginalUrl(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	urlRepo := NewUrlRepository(db)
 
 	tests := []struct {
 		Name          string
@@ -20,41 +28,30 @@ func TestGetOriginalUrlOk(t *testing.T) {
 		Setup         func(m sqlmock.Sqlmock)
 		ExpectOrigUrl string
 		ExpectErr     error
-		ExpectCode    int
 	}{
 		{
 			Name:     "successful getting origUrl",
-			ShortUrl: "Abc_efg",
+			ShortUrl: "Abc_efg_ag",
 			Setup: func(m sqlmock.Sqlmock) {
 				rows := m.NewRows([]string{"original_url"}).AddRow(
 					"http://ya.ru")
 				m.ExpectQuery(`SELECT original_url FROM url WHERE short_url=\$1`).WithArgs(
-					"Abc_efg").WillReturnRows(rows)
+					"Abc_efg_ag").WillReturnRows(rows)
 			},
 			ExpectOrigUrl: "http://ya.ru",
 			ExpectErr:     nil,
-			ExpectCode:    0,
 		},
 		{
 			Name:     "failed getting origUrl",
-			ShortUrl: "Abc_efah",
+			ShortUrl: "Abc_efah_a",
 			Setup: func(m sqlmock.Sqlmock) {
 				m.ExpectQuery(`SELECT original_url FROM url WHERE short_url=\$1`).WithArgs(
-					"Abc_efah").WillReturnError(sql.ErrNoRows)
+					"Abc_efah_a").WillReturnError(sql.ErrNoRows)
 			},
 			ExpectOrigUrl: "",
 			ExpectErr:     &utils.InternalError{Code: http.StatusNotFound, Message: "no originalUrl match this shortUrl"},
-			ExpectCode:    http.StatusConflict,
 		},
 	}
-
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Errorf("error while opening mock repo")
-	}
-	defer db.Close()
-
-	urlRepo := NewUrlRepository(db)
 
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
@@ -62,8 +59,87 @@ func TestGetOriginalUrlOk(t *testing.T) {
 			tt.Setup(mock)
 			res, err := urlRepo.GetOriginalUrl(context.Background(), tt.ShortUrl)
 
-			assert.Equal(t, res, tt.ExpectOrigUrl)
-			assert.Equal(t, err, tt.ExpectErr)
+			assert.Equal(t, tt.ExpectOrigUrl, res)
+			assert.Equal(t, tt.ExpectErr, err)
+		})
+	}
+
+}
+
+func TestAddOriginalUrl(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	urlRepo := NewUrlRepository(db)
+
+	tests := []struct {
+		Name      string
+		UrlData   models.UrlData
+		Setup     func(m sqlmock.Sqlmock, data models.UrlData)
+		ExpectErr error
+	}{
+		{
+			Name: "successful adding origUrl",
+			UrlData: models.UrlData{
+				ShortUrl:    "http://localhost:8080/Abc_def_gs",
+				OriginalUrl: "http://ya.ru",
+			},
+			Setup: func(m sqlmock.Sqlmock, data models.UrlData) {
+
+				m.ExpectQuery(`SELECT 1 FROM url WHERE short_url=\$1`).WithArgs(
+					data.ShortUrl).WillReturnError(sql.ErrNoRows)
+
+				m.ExpectExec(`INSERT INTO url \(short_url, original_url\) VALUES \(\$1, \$2\)`).WithArgs(
+					data.ShortUrl, data.OriginalUrl).WillReturnResult(sqlmock.NewResult(1, 1))
+
+			},
+			ExpectErr: nil,
+		},
+		{
+			Name: "error when origUrl already exists",
+			UrlData: models.UrlData{
+				ShortUrl:    "http://localhost:8080/Abc_def_gs",
+				OriginalUrl: "http://ya.ru",
+			},
+			Setup: func(m sqlmock.Sqlmock, data models.UrlData) {
+
+				rows := m.NewRows([]string{"1"}).AddRow(1)
+
+				m.ExpectQuery(`SELECT 1 FROM url WHERE short_url=\$1`).WithArgs(
+					data.ShortUrl).WillReturnRows(rows)
+
+			},
+			ExpectErr: &utils.InternalError{Code: http.StatusConflict, Message: "this shortUrl already exists"},
+		},
+		{
+			Name: "internal db error test",
+			UrlData: models.UrlData{
+				ShortUrl:    "http://localhost:8080/Abc_def_gs",
+				OriginalUrl: "http://ya.ru",
+			},
+			Setup: func(m sqlmock.Sqlmock, data models.UrlData) {
+
+				m.ExpectQuery(`SELECT 1 FROM url WHERE short_url=\$1`).WithArgs(
+					data.ShortUrl).WillReturnError(sql.ErrNoRows)
+
+				m.ExpectExec(`INSERT INTO url \(short_url, original_url\) VALUES \(\$1, \$2\)`).WithArgs(
+					data.ShortUrl, data.OriginalUrl).WillReturnError(fmt.Errorf("some bd error"))
+
+			},
+			ExpectErr: fmt.Errorf("pg.UrlRepository.AddOriginalUrl: %w", fmt.Errorf("some bd error")),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+
+			tt.Setup(mock, tt.UrlData)
+
+			err := urlRepo.AddOriginalUrl(context.Background(), &tt.UrlData)
+
+			assert.Equal(t, tt.ExpectErr, err)
 		})
 	}
 
